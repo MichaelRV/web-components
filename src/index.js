a22f78469be3dc4d6cb45692db473665981bb2a3
Michael Riabokon <michael.riabokon@gmail.com>
2021-06-25 16:14:07 +0300

    added isSafeInteger checking

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const resultEl = document.querySelector('#result');
 
 if (inputEl && buttonEl && resultEl) {
   buttonEl.addEventListener('click', () => {
+    if (!Number.isSafeInteger(+inputEl.value)) {
+      alert('This number is too big');
+
+      return;
+    }
+
     resultEl.innerHTML = (+inputEl.value * 2);
   });
 }
